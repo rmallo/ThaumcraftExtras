bb9c8c0fbbf3106ca6346b27c19bfb74ae413973
wasliebob <wasliebob@live.nl>
2013-11-05 17:07:02 +0100

    Base Work for Creative Tab Logo
    Prototype

--- thaumcraftextras/items/TCELogoItem.java
+++ thaumcraftextras/items/TCELogoItem.java
@@ -1,9 +1,26 @@
 package thaumcraftextras.items;
 
-public class TCELogoItem extends ItemTCE {
 
-public TCELogoItem(int id)
-{
-super(id);
+import thaumcraftextras.helpers.IconHelper;
+import thaumcraftextras.register.CreativeTabRegister;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.Item;
+
+
+public class TCELogoItem extends Item{
+
+
+        public TCELogoItem(int id) {
+                super(id);
+        }
+        
+        @Override
+    public void registerIcons(IconRegister par1IconRegister) 
+        {
+        itemIcon = IconHelper.forItem(par1IconRegister, this);
 }
+
+
+        
 }
+
